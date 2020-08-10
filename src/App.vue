3733c5f66df6f7d2422d6c294fb6adca66fb8292
Bingyilin <kino147258@gmail.com>
2020-08-10 20:30:27 +0800

    Initialization

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png">
+    <QuoteGrid :quotes="quotes"/>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid";
+export default {
+  name: 'App',
+  data: function (){
+    return{
+      quotes:[
+        'Just a Quote to see something',
+        'Hello World',
+        'Hello World',
+        'Hello World',
+        'Just a Quote to see something'
+      ],
+      maxQuotes: 10
+    }
+  },
+  components: {
+    QuoteGrid
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+      <Quote v-for="quote in quotes">{{ quote }}</Quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote";
+export default {
+  name: "QuoteGrid",
+  // 自訂一properties，可以讓你從parent注入時候定義屬性帶data到此template/component之中
+  props: ['quotes'],
+  data: function (){
+    return{
+      name: 'Dante',
+      info: 'test'
+    }
+  },
+  components:{
+    Quote
+  }
+}
+</script>
+
+<style>
+</style>
